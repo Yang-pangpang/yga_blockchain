93e031dff60c89addc56c2600eee50b33b96d0ce
杨国安 <530037146@qq.com>
2024-11-18 17:29:05 +0800

    install cli-table

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 const vorpal = require('vorpal')();
 const BlockChain = require('./blockChain');
-const blockchain = new BlockChain()
+const Tbale = require('cli-table');
+const blockchain = new BlockChain();
+
+
+function formatLog(data) {
+    if (!Array.isArray((data))) {
+        data = [data];
+    }
+    const first = data[0];
+    const head = Object.keys(first);
+    const table = new Tbale({
+        head: head,
+        colWidths: new Array(head.length).fill(30)
+    });
+
+    const result = data.map(item => {
+        return head.map(v => item[v])
+    })
+    table.push(...result);
+    console.log(table.toString());
+}
+
 
 // vorpal
 //     .command('Welcome yga_blockchain')
@@ -10,19 +31,19 @@ const blockchain = new BlockChain()
 //     });
 
 vorpal
-    .command('mine','挖矿')
-    .action(function(args, callback) {
+    .command('mine', '挖矿')
+    .action(function (args, callback) {
         const newBlock = blockchain.mine();
-        if(newBlock) {
-            console.log(newBlock);
+        if (newBlock) {
+            formatLog(newBlock);
         }
         callback();
     });
 
 vorpal
-    .command('chain','查看区块链')
-    .action(function(args, callback) {
-       this.log(blockchain.blockchain);
+    .command('chain', '查看区块链')
+    .action(function (args, callback) {
+        formatLog(blockchain.blockchain);
         callback();
     });
 
